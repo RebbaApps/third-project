a0206db0c72f6213df9ff24f1445329fe0d00d58
praveen <praveen.varaprasad@gmail.com>
2024-02-29 15:06:24 +0530

    Third Project

---
+++ app/src/main/java/com/synycs/test3/MainActivity.java
@@ -0,0 +1,17 @@
+package com.synycs.test3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "Project 3", Toast.LENGTH_SHORT).show();
+    }
+}
